99d1969152851ae12c6c9ded7b63da63adff1a49
david <david@usuario-OptiPlex-3050>
2020-02-12 09:10:25 +0100

    Clase Personaje

--- JuegoRol/src/enemigos/Enemigo.java
+++ JuegoRol/src/enemigos/Enemigo.java
@@ -1,11 +1,19 @@
 package enemigos;
 
-public class Enemigo {
-	private int vida;
-	private int dano;
-	
-	public Enemigo(int health) {
-		this.vida = health;
 
+public class Enemigo {
+	private int constitucion;
+	private int vidaActual;
+	private int fuerza;
+	private int destreza;
+	private int defensa;
+	private int resistencia;
+	
+	public Enemigo() {
+		vidaActual=constitucion;
+	}
+	
+	public void recibeDano(int ataque) {
+		vidaActual-=(ataque-ataque*defensa/100);
 	}
 }
--- JuegoRol/src/personajes/Personaje.java
+++ JuegoRol/src/personajes/Personaje.java
@@ -9,24 +9,26 @@ public class Personaje {
 	private int destreza;
 	private int defensa;
 	private int resistencia;
-	private Objeto[] objetos=new Objeto[5];
+	private Objeto[] objetos = new Objeto[5];
 	private int priPosLib;
-	
-	public Personaje() {
-		vidaActual=constitucion;
+
+	public Personaje(int cons, int fuer, int dest, int def, int rest) {
+		constitucion = cons;
+		fuerza = fuer;
+		destreza = dest;
+		defensa = def;
+		destreza = dest;
+		vidaActual = constitucion;
+
 	}
-	
+
 	public void recibeDano(int ataque) {
-		vidaActual-=(ataque-ataque*defensa/100);
+		vidaActual -= (ataque - ataque * defensa / 100);
 	}
-	
+
 	public void equipaObjeto(Objeto item) {
-		objetos[priPosLib]=item;
+		objetos[priPosLib] = item;
 		priPosLib++;
 	}
-	
 
-	
-	
-	
 }
