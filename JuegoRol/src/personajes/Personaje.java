8afaeca282a713a2623b9e62ddf44173e7d2093d
usuario <usuario@usuario06>
2020-02-12 08:58:30 +0100

    clase personaje

--- JuegoRol/src/personajes/Personaje.java
+++ JuegoRol/src/personajes/Personaje.java
@@ -1,5 +1,32 @@
 package personajes;
 
-public class Personaje {
+import objetos.Objeto;
 
+public class Personaje {
+	private int constitucion;
+	private int vidaActual;
+	private int fuerza;
+	private int destreza;
+	private int defensa;
+	private int resistencia;
+	private Objeto[] objetos=new Objeto[5];
+	private int priPosLib;
+	
+	public Personaje() {
+		vidaActual=constitucion;
+	}
+	
+	public void recibeDano(int ataque) {
+		vidaActual-=(ataque-ataque*defensa/100);
+	}
+	
+	public void equipaObjeto(Objeto item) {
+		objetos[priPosLib]=item;
+		priPosLib++;
+	}
+	
+
+	
+	
+	
 }
