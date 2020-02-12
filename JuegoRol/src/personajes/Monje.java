ca7fef1acba4c7ed5ffa0c591c5486127d8b9daa
usuario <usuario@usuario06>
2020-02-12 09:08:42 +0100

    asd

--- JuegoRol/src/personajes/Monje.java
+++ JuegoRol/src/personajes/Monje.java
@@ -1,5 +1,33 @@
 package personajes;
 
-public class Monje {
+public class Monje extends Personaje implements Ataque{
+
+	
+	public Monje() {
+		
+	}
+	
+	public int realizaAtaque1() {
+
+		return 0;
+	}
+
+	
+	public int realizaAtaque2() {
+
+		return 0;
+	}
+
+	
+	public int realizaAtaque3() {
+
+		return 0;
+	}
+
+	
+	public int realizaAtaque4() {
+
+		return 0;
+	}
 
 }
