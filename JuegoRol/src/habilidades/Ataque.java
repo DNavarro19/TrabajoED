321e37a2c25be3f7b0c3b8bdb0acfbc6289dd53f
david <david@usuario-OptiPlex-3050>
2020-02-18 14:45:55 +0100

    Ataque

--- JuegoRol/src/habilidades/Ataque.java
+++ JuegoRol/src/habilidades/Ataque.java
@@ -2,5 +2,12 @@ package habilidades;
 
 public class Ataque {
 	private int dano;
-	
+	private boolean aturde;
+	private int ignoraDefensa;
+	private boolean esquiva;
+
+	public Ataque(int dano, boolean aturde) {
+		this.dano = dano;
+		this.aturde = aturde;
+	}
 }
