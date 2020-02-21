9e46aafc17936a99d556c22434a6f3d0d084ec70
Usuario <Usuario@172.16.17.75>
2020-02-21 08:59:46 +0100

    implementacion de metodos set para añadir cantidades segun los objetos

--- JuegoRol/src/personajes/Personaje.java
+++ JuegoRol/src/personajes/Personaje.java
@@ -2,7 +2,6 @@ package personajes;
 
 import objetos.Objeto;
 
-
 public class Personaje {
 	private int constitucion;
 	private int fuerza;
@@ -25,29 +24,63 @@ public class Personaje {
 	public void recibeDano(int ataque) {
 		vidaActual -= (ataque - ataque * defensa / 100);
 	}
-/**
- * 
- * @param item el objeto que usa el pesonajes
- * @param p el personaje que usa el objeto
- * @return equipa el objeto en un array o no lo equipa porque el array esta lleno
- * 
- */
-	public void equipaObjeto(String tipo,Personaje p) {
+
+	/**
+	 * 
+	 * @param item el objeto que usa el pesonajes
+	 * @param p    el personaje que usa el objeto
+	 * @return equipa el objeto en un array o no lo equipa porque el array esta
+	 *         lleno
+	 * 
+	 */
+	public void equipaObjeto(String tipo, Personaje p) {
 		if (priPosLib < Objetos.length) {
-			Objeto obj=new Objeto(tipo);
-			obj.usaObjeto(tipo,p);
+			Objeto obj = new Objeto(tipo);
+			obj.usaObjeto(tipo, p);
 			Objetos[priPosLib] = obj;
 			priPosLib++;
 		} else {
 			System.out.println("Ya no puedes equiparte mas objetos");
 		}
 	}
+
 	/**
 	 * 
-	 * @param cantidad la cantidad nueva de constitucion
+	 * @param cantidad la cantidad de constitucion a añadir
 	 */
 	public void setCostitucion(int cantidad) {
-		this.constitucion=cantidad;
+		this.constitucion += cantidad;
+	}
+	/**
+	 * 
+	 * @param fuerza la cantidad de fuerza a añadir
+	 */
+
+	public void setFuerza(int fuerza) {
+		this.fuerza += fuerza;
+	}
+	/**
+	 * 
+	 * @param destreza la cantidad de destreza a añadir
+	 */
+
+	public void setDestreza(int destreza) {
+		this.destreza += destreza;
+	}
+	/**
+	 * 
+	 * @param defensa la cantidad de defensa a añadir
+	 */
+	public void setDefensa(int defensa) {
+		this.defensa += defensa;
+	}
+	/**
+	 * 
+	 * @param resistencia la cantidad de resistencia a añadir
+	 */
+
+	public void setResistencia(int resistencia) {
+		this.resistencia += resistencia;
 	}
 
 }
