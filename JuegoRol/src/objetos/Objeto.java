c05859c2582a03b5abb47c8758dd6f5012b56acf
Usuario <Usuario@172.16.17.75>
2020-02-21 09:13:35 +0100

    Cambios en objetos

--- JuegoRol/src/objetos/Objeto.java
+++ JuegoRol/src/objetos/Objeto.java
@@ -8,7 +8,7 @@ public class Objeto  {
 		this.tipo=tipo;
 	}
 	/**
-	 * 
+	 * metodo que modifica los atributos del personaje segun el objeto a utilizar
 	 * @param tipos el tipo de objeto que se quiere usar
 	 * @param p El personaje al que se le aplica el objeto
 	 */
@@ -16,19 +16,32 @@ public class Objeto  {
 		switch (tipos) {
 		case "amuleto":
 			p.setCostitucion(70);
+			//añadir la cantidad deseada al atributo deseado
 			break;
 		case "anillo":
 			p.setCostitucion(40);
+			//añadir la cantidad deseada al atributo deseado
 			break;
 		case "collar":
 			p.setCostitucion(14);
+			//añadir la cantidad deseada al atributo deseado
 		default:
 			break;
 		}
 	}
-	//metodo que 
+/**
+ * metodo que muestra el tipo de objeto
+ */
 	public void muestraObjeto() {
 		System.out.println(this.tipo);
+	}
+	/**
+	 * metodo que muestra los atributos que cambia cada objeto
+	 */
+	public void describeObjetos() {
+		System.out.println("Amuleto:\n>aumenta el atributo en X puntos.\n>disminuye el atributo en X puntos");
+		System.out.println("Collar:\n>aumenta el atributo en X puntos.\n>disminuye el atributo en X puntos");
+		System.out.println("Anillo:\n>aumenta el atributo en X puntos.\n>disminuye el atributo en X puntos");
 	}
 	
 }
