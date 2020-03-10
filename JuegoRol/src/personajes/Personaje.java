a6b7e086eead5499b92a109040a307256972c206
david <david@usuario-OptiPlex-3050>
2020-03-10 14:22:36 +0100

    Personae

--- JuegoRol/src/personajes/Personaje.java
+++ JuegoRol/src/personajes/Personaje.java
@@ -17,6 +17,23 @@ public class Personaje {
 	protected Objeto[] objetos = new Objeto[5];
 	protected int priPosLib;
 
+	/**
+	 * @param cons Valor de la constitucion
+	 * @param fuer Valor de la fuerza
+	 * @param dest Valor de la destreza
+	 * @param def  Valor de la defensa
+	 * @param rest Valor de la resistencia
+	 * 
+	 *             Por defecto se estableceran estos atributos:
+	 *             <ul>
+	 *             <li>Vida actual sera igual a la constitucion</li>
+	 *             <li>Resistencia actual sera igual a la resistencia</li>
+	 *             <li>Defensa extra sera 0 por defecto</li>
+	 *             <li>Fuerza extra sera 0 por defecto</li>
+	 *             <li>Aturdido sera falso por defecto</li>
+	 *             <li>Esquiva sera falso por defecto</li>
+	 *             </ul>
+	 */
 	public Personaje(int cons, int fuer, int dest, int def, int rest) {
 		constitucion = cons;
 		fuerza = fuer;
@@ -35,62 +52,87 @@ public class Personaje {
 	 * Este metodo indica la cantidad numerica que va a restarse a la vida actual
 	 * del personaje
 	 * 
-	 * @param ataque cantidad de dano que va a recibir
+	 * @param ataque Cantidad de dano que va a recibir el personaje
 	 * 
 	 */
 	public void recibeDano(int ataque) {
 		vidaActual -= (ataque - ataque * (defensa + defensaExtra) / 100);
 	}
 
+	/**
+	 * @param cura
+	 */
 	public void recibeVida(int cura) {
 		vidaActual += cura;
 	}
 
+	/**
+	 * @return Devuelve el valor de la constitucion que tiene el personaje
+	 */
 	public int getConstitucion() {
 		return constitucion;
 	}
 
+	/**
+	 * @return Devuelve el valor de la fuerza que tiene el personaje
+	 */
 	public int getFuerza() {
 		return fuerza;
 	}
 
+	/**
+	 * @return Devuelve el valor de la destreza que tiene el personaje
+	 */
 	public int getDestreza() {
 		return destreza;
 	}
 
+	/**
+	 * @return Devuelve el valor de la defensa que tiene el personaje
+	 */
 	public int getDefensa() {
 		return defensa;
 	}
 
+	/**
+	 * @return Devuelve el valor de la resistencia que tiene el personaje
+	 */
 	public int getResistencia() {
 		return resistencia;
 	}
 
+	/**
+	 * @return Devuelve el valor de la vida actual que tiene el personaje
+	 */
 	public int getVidaActual() {
 		return vidaActual;
 	}
 
+	/**
+	 * @return Devuelve los objetos que tiene equipados el personaje
+	 */
 	public Objeto[] getObjetos() {
 		return objetos;
 	}
 
+	/**
+	 * @return Devuelve el valor de la primera posicion libre del array de objetos
+	 *         del personaje
+	 */
 	public int getPriPosLib() {
 		return priPosLib;
 	}
 
 	/**
 	 * <ul>
-	 * <li>equipa el objeto en un array</li>
-	 * <li>no lo equipa porque el array esta lleno</li>
+	 * <li>Equipa el objeto en un array</li>
+	 * <li>No lo equipa porque el array esta lleno</li>
 	 * </ul>
 	 * 
-	 * @param tipo el objeto que usa el personaje
-	 * @param p    el personaje que usa el objeto
-	 * 
-	 * 
+	 * @param tipo El objeto que usa el personaje
 	 * 
 	 */
-	public void equipaObjeto(Objeto obj, Personaje p) {
+	public void equipaObjeto(Objeto obj) {
 		if (priPosLib < objetos.length) {
 			usaObjeto(obj);
 			objetos[priPosLib] = obj;
@@ -101,12 +143,11 @@ public class Personaje {
 	}
 
 	/**
-	 * metodo que modifica los atributos del personaje segun el objeto a utilizar
+	 * Metodo que modifica los atributos del personaje segun el objeto a utilizar
 	 * 
 	 * @param tipos el tipo de objeto que se quiere usar
-	 * @param p     El personaje al que se le aplica el objeto
 	 */
-	public void usaObjeto(Objeto obj) {
+	private void usaObjeto(Objeto obj) {
 		switch (obj.getCaracteristica()) {
 		case "constitucion":
 			constitucion += obj.getPuntos();
@@ -129,53 +170,56 @@ public class Personaje {
 	}
 
 	/**
-	 * 
-	 * @param cantidad la cantidad de constitucion a anadir
+	 * @param constitucion Cantidad de constitucion a anadir
 	 */
-	public void setConstitucion(int cantidad) {
-		this.constitucion += cantidad;
+	public void anadeConstitucion(int constitucion) {
+		this.constitucion += constitucion;
 	}
 
+	/**
+	 * @param aturdido Cambia el valor de aturdido
+	 */
 	public void setAturdido(boolean aturdido) {
 		this.aturdido = aturdido;
 	}
 
+	/**
+	 * @param esquiva Cambia el valor de esquiva
+	 */
 	public void setEsquiva(boolean esquiva) {
 		this.esquiva = esquiva;
 	}
 
 	/**
-	 * 
-	 * @param fuerza la cantidad de fuerza a anadir
+	 * @param fuerza Cantidad de fuerza a anadir
 	 */
-
-	public void setFuerza(int fuerza) {
+	public void anadeFuerza(int fuerza) {
 		this.fuerza += fuerza;
 	}
 
 	/**
 	 * 
-	 * @param destreza la cantidad de destreza a anadir
+	 * @param destreza Cantidad de destreza a anadir
 	 */
 
-	public void setDestreza(int destreza) {
+	public void anadeDestreza(int destreza) {
 		this.destreza += destreza;
 	}
 
 	/**
 	 * 
-	 * @param defensa la cantidad de defensa a anadir
+	 * @param defensa Cantidad de defensa a anadir
 	 */
-	public void setDefensa(int defensa) {
+	public void anadeDefensa(int defensa) {
 		this.defensa += defensa;
 	}
 
 	/**
 	 * 
-	 * @param resistencia la cantidad de resistencia a anadir
+	 * @param resistencia Cantidad de resistencia a anadir
 	 */
 
-	public void setResistencia(int resistencia) {
+	public void anadeResistencia(int resistencia) {
 		this.resistencia += resistencia;
 	}
 
