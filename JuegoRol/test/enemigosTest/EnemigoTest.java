7ca719ee8cdca2b9d3e7d354ef7e52ac5f96b8c5
usuario <usuario@usuario06>
2020-03-11 08:58:49 +0100

    Finalizacion de pruebas de enemigo

--- JuegoRol/test/enemigosTest/EnemigoTest.java
+++ JuegoRol/test/enemigosTest/EnemigoTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import enemigos.Enemigo;
-import personajes.Personaje;
+import personajes.Monje;
 
 class EnemigoTest {
 
@@ -17,15 +17,51 @@ class EnemigoTest {
 		enemy.recibeDano(dano);
 		int actual=enemy.getVidaActual();
 		assertEquals(expected,actual,"Resta una cantidad de dano a la vida actual teniendo en cuenta la defensa");
-		
 	}
 	
 	@Test
-	public void testAtaqueBasico(Personaje p) {
+	public void testAtaqueBasico() {
 		Enemigo enemy=new Enemigo(50,50,50,50);
-		int expected=50+50*(enemy.getFuerza()+enemy.getFuerzaExtra())/100;
-		int actual=50+50*(50+0)/100;
-		assertEquals(expected,actual,"La cantidad de dano que va a realizar el ataque basico");
+		Monje monk=new Monje();
+		enemy.ataqueBasico(monk);
+		int dano=50+50*(enemy.getFuerza()+enemy.getFuerzaExtra())/100;
+		int actual=monk.getVidaActual();
+		int expected=monk.getConstitucion()-dano;
+	}
+	
+	@Test
+	public void testRecibeVida() {
+		Enemigo enemy=new Enemigo(50,50,50,50);
+		enemy.setVidaActual(10);
+		int cura=30;
+		enemy.recibeVida(cura);
+		int actual=enemy.getVidaActual();
+		int expected=10+30;
+		assertEquals(expected,actual,"La vida debe estar a 40");
+	}
+	
+	@Test
+	public void testRecibeVida2() {
+		Enemigo enemy=new Enemigo(50,50,50,50);
+		int cura=30;
+		enemy.recibeVida(cura);
+		int actual=enemy.getVidaActual();
+		int expected=50;
+		assertEquals(expected,actual,"La vida debe estar a 50, el valor de la constitucion");
 	}
 
+	@Test
+	public void testReset() {
+		Enemigo enemy=new Enemigo(50,50,50,50);
+		enemy.setFuerzaExtra(50);
+		enemy.setDefensaExtra(50);
+		enemy.reset();
+		int fActual=enemy.getFuerzaExtra();
+		int dActual=enemy.getDefensaExtra();
+		int fExpected=0;
+		int dExpected=0;
+		assertEquals(fExpected, fActual,"La fuerza extra debe estar a 0");
+		assertEquals(dExpected, dActual,"La defensa extra debe estar a 0");
+	}
+	
 }
