df8069311a4d61b2a049f595d789d205484b639f
pablete <pablete@192.168.56.1>
2020-03-30 18:45:40 +0200

    test Menu

--- JuegoRol/test/menuTest/MenuTest.java
+++ JuegoRol/test/menuTest/MenuTest.java
@@ -7,6 +7,8 @@ import java.io.PrintStream;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import menu.Menu;
@@ -18,13 +20,13 @@ class MenuTest {
 	private final PrintStream originalOut = System.out;
 	private final PrintStream originalErr = System.err;
 
-	@Before
+	@BeforeEach
 	public void setUpStreams() {
 	    System.setOut(new PrintStream(outContent));
 	    System.setErr(new PrintStream(errContent));
 	}
 
-	@After
+	@AfterEach
 	public void restoreStreams() {
 	    System.setOut(originalOut);
 	//    System.setErr(originalErr);
@@ -35,7 +37,7 @@ class MenuTest {
 	void opcionesSalaTest() {
 		 Menu target=new Menu();
 		 target.opcionesSala();
-		 assertEquals("1)siguiente sala\\n2)mostrar vid", outContent.toString());
+		 assertEquals("1)siguiente sala\n2)mostrar vida", outContent.toString());
 	}
 
 }
